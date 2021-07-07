f838b5cdc110f36f2a338967d8c792b3d58e5ac6
Vijay <vijay.v@chapter247.com>
2021-07-07 10:32:59 +0530

    Layout of Music247

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,103 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<v-app class="dark">
+    <v-app-bar flat app dark color="dark">
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer" class="d-lg-none"></v-app-bar-nav-icon>
+
+        <v-card-title class="white--text d-lg-none font--subtitle--2">
+            Songs247x7
+        </v-card-title>
+
+        <v-text-field append-icon="mdi-magnify" placeholder="Search" filled dense rounded class="mt-6 d-none d-sm-flex"></v-text-field>
+
+        <!-- <v-spacer></v-spacer> -->
+        <v-spacer></v-spacer>
+        <v-spacer></v-spacer>
+
+        <v-menu offset-y>
+            <template v-slot:activator="{ on, attrs }">
+                <v-btn text class="profile--btn text-none" color="dark" rounded v-bind="attrs" v-on="on">
+                    <v-avatar size="20" left>
+                        <v-img src="./assets/logo.png" />
+                    </v-avatar>
+                    <span class="ml-2">Profile</span>
+                    <v-icon right size="24">mdi-chevron-down</v-icon>
+                </v-btn>
+            </template>
+            <v-list color="dark" dark>
+                <v-list-item v-for="(item, index) in items" :key="index" route :to="item.route">
+                    <v-list-item-title class="profile--text--dropdown">{{
+              item.title
+            }}</v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-menu>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" app dark color="dark" class="white--text">
+        <v-card-title class="dark">
+            Songs247x7
+        </v-card-title>
+
+        <v-text-field class="d-sm-none" placeholder="Search" append-icon="mdi-magnify" filled dense rounded></v-text-field>
+    </v-navigation-drawer>
+    <v-main>
+        <router-view />
+    </v-main>
+</v-app>
 </template>
 
+<script>
+export default {
+    name: "App",
+    data: () => ({
+        drawer: true,
+        items: [{
+                title: "Account",
+                route: "/account",
+            },
+            {
+                title: "Log Out",
+                route: "/logout",
+            },
+        ],
+    }),
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@media only screen and (max-width: 624px) {
+    .font--subtitle--2 {
+        font-size: 0.875rem !important;
+        font-weight: 500 !important;
+        letter-spacing: 0.0071428571em !important;
+        line-height: 1.375rem !important;
+        font-family: "Roboto", sans-serif !important;
+    }
+
+    .profile--btn {
+        font-size: 0.75rem !important;
+    }
+
+    .profile--text--dropdown {
+        font-size: 0.85rem !important;
+    }
 }
 
-#nav {
-  padding: 30px;
+.dark {
+    background: #17212d !important;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.v-text-field.v-input--dense:not(.v-text-field--outlined) .v-text-field__prefix,
+.v-text-field.v-input--dense:not(.v-text-field--outlined) .v-text-field__suffix,
+.v-text-field.v-input--dense:not(.v-text-field--outlined) input {
+    padding: 0px !important;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.v-input__icon--append .v-icon {
+    color: #94999e !important;
+}
+
+.v-navigation-drawer__border {
+    background-color: #17212d !important;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container fluid class="dark">
+    <v-card dark flat class="dark">
+      <v-card-title class="text-h4">
+        Discover
+      </v-card-title>
+
+      <v-card-text>
+        <v-row>
+          <v-col cols="8">
+            <v-card dark class="dark rounded-lg">
+              <v-img
+                height="280"
+                src="https://media.istockphoto.com/photos/the-african-king-picture-id492611032?k=6&m=492611032&s=612x612&w=0&h=Y_CHJCbkFOqmXvHOYSdxu0T5UZqoRj7OzpIBLGkvf_Q="
+              ></v-img>
+            </v-card>
+          </v-col>
+          <v-col cols="4">
+            <v-card dark class="dark rounded-lg">
+              <v-img
+                height="280"
+                src="https://media.istockphoto.com/photos/the-african-king-picture-id492611032?k=6&m=492611032&s=612x612&w=0&h=Y_CHJCbkFOqmXvHOYSdxu0T5UZqoRj7OzpIBLGkvf_Q="
+              ></v-img>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-card-text>
+    </v-card>
+  </v-container>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
