eb19dfce8ab8bd06f255400499fd53060ae8c661
Vijay <vijay.v@chapter247.com>
2021-07-08 10:40:36 +0530

    Drawer,s Menu & Artist Buttons

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,85 @@
 <template>
 <v-app class="dark">
+    <!-- Navigation Bar -->
     <v-app-bar flat app dark color="dark">
         <v-app-bar-nav-icon @click.stop="drawer = !drawer" class="d-lg-none"></v-app-bar-nav-icon>
-
-        <v-card-title class="white--text d-lg-none font--subtitle--2">
+        <v-card-title class="d-lg-none font-weight-bold font--subtitle--2  white--text">
             Songs247x7
         </v-card-title>
 
-        <v-text-field append-icon="mdi-magnify" placeholder="Search" filled dense rounded class="mt-6 d-none d-sm-flex"></v-text-field>
+        <v-text-field append-icon="mdi-magnify" placeholder="Search" filled dense rounded class="d-none d-sm-flex mt-6"></v-text-field>
 
-        <!-- <v-spacer></v-spacer> -->
         <v-spacer></v-spacer>
         <v-spacer></v-spacer>
 
+        <!-- Profile Button -->
         <v-menu offset-y>
             <template v-slot:activator="{ on, attrs }">
                 <v-btn text class="profile--btn text-none" color="dark" rounded v-bind="attrs" v-on="on">
-                    <v-avatar size="20" left>
-                        <v-img src="./assets/logo.png" />
+                    <v-avatar size="25" left>
+                        <v-img src="./assets/image_14.png" />
                     </v-avatar>
-                    <span class="ml-2">Profile</span>
+                    <span class="font-weight-bold ml-2">Profile</span>
                     <v-icon right size="24">mdi-chevron-down</v-icon>
                 </v-btn>
             </template>
-            <v-list color="dark" dark>
-                <v-list-item v-for="(item, index) in items" :key="index" route :to="item.route">
-                    <v-list-item-title class="profile--text--dropdown">{{
-              item.title
-            }}</v-list-item-title>
+            <v-list rounded color="dark" dark>
+                <v-list-item v-for="(item, index) in profile" :key="index" route :to="item.route">
+                    <v-list-item-content>
+                        <v-list-item-title class="font-weight-bold profile--text--dropdown text-center">{{ item.title }}</v-list-item-title>
+                    </v-list-item-content>
                 </v-list-item>
             </v-list>
         </v-menu>
     </v-app-bar>
 
+    <!-- Drawer Content -->
     <v-navigation-drawer v-model="drawer" app dark color="dark" class="white--text">
-        <v-card-title class="dark">
+        <v-card-title class="dark font-weight-bold">
             Songs247x7
         </v-card-title>
 
-        <v-text-field class="d-sm-none" placeholder="Search" append-icon="mdi-magnify" filled dense rounded></v-text-field>
+        <v-text-field class="d-sm-none mx-4 my-0 py-0" placeholder="Search" append-icon="mdi-magnify" filled dense rounded></v-text-field>
+
+        <v-subheader class="font-weight-black grey--text">Menu</v-subheader>
+
+        <!-- Menu Section -->
+        <v-btn text block v-for="item in menu" :key="item.text" :to="item.route" class="body-2 py-5 text-none ">
+            <span class="bg--grey grey--light--text mr-5 pa-1 rounded-lg">
+                <v-icon size="18" class="icon--white grey--icon" v-text="item.icon"></v-icon>
+            </span>
+            <span class="font-weight-medium grey--text text" v-text="item.text">
+            </span>
+            <v-spacer></v-spacer>
+        </v-btn>
+
+        <v-row justify="center">
+            <v-col cols="10">
+                <v-divider class="my-5"></v-divider>
+            </v-col>
+        </v-row>
+
+        <!-- Artist Section -->
+        <v-subheader class="font-weight-black grey--text">Artist</v-subheader>
+
+        <v-btn text block v-for="(artist, index) in artists" :key="index" :to="artist.route" class="body-2 py-5 text-none ">
+            <span class="bg--grey grey--light--text mr-5 pa-1 rounded-lg ">
+                <v-icon size="18" class="icon--white grey--icon" v-text="artist.icon"></v-icon>
+            </span>
+            <span class="font-weight-medium grey--text text" v-text="artist.text">
+            </span>
+            <v-spacer></v-spacer>
+        </v-btn>
+
+        <v-row justify="center">
+            <v-col cols="10">
+                <v-divider class="my-5"></v-divider>
+            </v-col>
+        </v-row>
+
     </v-navigation-drawer>
+
+    <!-- Router Content -->
     <v-main>
         <router-view />
     </v-main>
@@ -51,13 +91,71 @@ export default {
     name: "App",
     data: () => ({
         drawer: true,
-        items: [{
+        profile: [{
                 title: "Account",
                 route: "/account",
             },
             {
                 title: "Log Out",
                 route: "/logout",
+            },
+        ],
+        setId: 1,
+        setArtistId: null,
+        menu: [{
+                id: 1,
+                icon: "mdi-home",
+                text: "Home",
+                route: "/",
+            },
+            {
+                id: 2,
+                icon: "mdi-file-check",
+                text: "Trending",
+                route: "/trending",
+            },
+            {
+                id: 3,
+                icon: "mdi-nutrition",
+                text: "Playlist",
+                route: "/playlist",
+            },
+            {
+                id: 4,
+                icon: "mdi-library",
+                text: "Albums",
+                route: "/album",
+            },
+            {
+                id: 5,
+                icon: "mdi-clipboard-check",
+                text: "Profile",
+                route: "/profile",
+            },
+        ],
+        artists: [{
+                id: 1,
+                icon: "mdi-home",
+                text: "Arijit Singh",
+                route: "/arijit",
+            },
+            {
+                id: 2,
+                icon: "mdi-file-check",
+                text: "KAKA",
+                route: "/kaka",
+            },
+            {
+                id: 3,
+                icon: "mdi-nutrition",
+                text: "Arman Malik",
+                route: "/arman",
+            },
+            {
+                id: 4,
+                icon: "mdi-library",
+                text: "K.K.",
+                route: "/kk",
             },
         ],
     }),
@@ -93,11 +191,45 @@ export default {
     padding: 0px !important;
 }
 
-.v-input__icon--append .v-icon {
+.v-input__icon--append .v-icon,
+.grey--text {
+    color: #94999e !important;
+}
+
+.grey--light--text {
+    background-color: #29323d !important;
+}
+
+.grey--icon {
     color: #94999e !important;
 }
 
 .v-navigation-drawer__border {
     background-color: #17212d !important;
 }
+
+.v-list-item__content {
+    padding: 0 !important;
+}
+
+.pr-100 {
+    padding-right: 200px !important;
+}
+
+.theme--dark.v-btn--active:hover::before,
+.theme--dark.v-btn--active::before {
+    opacity: 0 !important;
+}
+
+.v-btn--active span .text {
+    color: white !important;
+    font-weight: 900 !important;
+}
+
+.v-btn--active span .bg--grey,
+.v-btn--active span .icon--white {
+    background-color: #ff7300 !important;
+    color: white !important;
+}
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,76 @@
 <template>
-  <v-container fluid class="dark">
+<v-container fluid class="dark mt-n5">
     <v-card dark flat class="dark">
-      <v-card-title class="text-h4">
-        Discover
-      </v-card-title>
-
-      <v-card-text>
-        <v-row>
-          <v-col cols="8">
-            <v-card dark class="dark rounded-lg">
-              <v-img
-                height="280"
-                src="https://media.istockphoto.com/photos/the-african-king-picture-id492611032?k=6&m=492611032&s=612x612&w=0&h=Y_CHJCbkFOqmXvHOYSdxu0T5UZqoRj7OzpIBLGkvf_Q="
-              ></v-img>
-            </v-card>
-          </v-col>
-          <v-col cols="4">
-            <v-card dark class="dark rounded-lg">
-              <v-img
-                height="280"
-                src="https://media.istockphoto.com/photos/the-african-king-picture-id492611032?k=6&m=492611032&s=612x612&w=0&h=Y_CHJCbkFOqmXvHOYSdxu0T5UZqoRj7OzpIBLGkvf_Q="
-              ></v-img>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-card-text>
+        <v-card-title class="font-weight-bold text-h5">
+            Discover
+        </v-card-title>
     </v-card>
-  </v-container>
+
+    <!-- <v-row justify="start">
+        <v-col cols="6">
+            <v-card class="rounded-xl">
+                <v-img src=".././assets/image_1.png">
+
+                    <span class="font-weight-bold white--text text-h5">How to do Basic Jumping and how to landing safely</span>
+
+                    <v-card flat class="transparent">
+                        <v-row>
+                            <v-col cols="6" class="border">
+                                <v-card-title>
+                                    <p class="font-weight-bold white--text text-justify text-h4 mt-6 pa-2">
+                                        How to do Basic Jumping and how to landing safely
+                                    </p>
+                                </v-card-title>
+                            </v-col>
+
+                            <v-col cols="12">
+                                <v-card-title>
+                                    <v-avatar size="56" class="round--circle">
+                                        <img alt="user" src=".././assets/image_2.png">
+                                    </v-avatar>
+
+                                    <v-row>
+                                        <v-col cols="12">
+                                            <p class="ml-3 white--text">
+                                                Thomas Hope
+                                            </p>
+                                        </v-col>
+                                    </v-row>
+                                </v-card-title>
+                            </v-col>
+
+                            <v-col cols="12">
+                                <v-card-actions>
+                                    <v-spacer></v-spacer>
+                                    <v-chip color="#3d414d" class="white--text font-weight-bold body-1 ma-5">
+                                        7:10 min
+                                    </v-chip>
+                                </v-card-actions>
+                            </v-col>
+                        </v-row>
+                    </v-card>
+                </v-img>
+            </v-card>
+        </v-col>
+    </v-row> -->
+
+
+
+</v-container>
 </template>
+
+<style scoped>
+.transparent {
+    background-color: transparent !important;
+}
+
+/*
+.border {
+    border: 1px solid red;
+} */
+
+/* .round--circle {
+    border: 1px solid grey;
+
+} */
+</style>
