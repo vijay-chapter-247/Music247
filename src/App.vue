dcfdddd895dfcf9573cd008fd1dd402887f6c986
Vijay <vijay.v@chapter247.com>
2021-07-08 20:20:40 +0530

    Create Cards

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <!-- Navigation Bar -->
     <v-app-bar flat app dark color="dark">
         <v-app-bar-nav-icon @click.stop="drawer = !drawer" class="d-lg-none"></v-app-bar-nav-icon>
-        <v-card-title class="d-lg-none font-weight-bold font--subtitle--2  white--text">
+        <v-card-title class="d-lg-none font-weight-bold font--subtitle--2 font-family--poppins white--text">
             Songs247x7
         </v-card-title>
 
@@ -15,18 +15,18 @@
         <!-- Profile Button -->
         <v-menu offset-y>
             <template v-slot:activator="{ on, attrs }">
-                <v-btn text class="profile--btn text-none" color="dark" rounded v-bind="attrs" v-on="on">
+                <v-btn text class="profile--btn text-none font-family--poppins" color="dark" rounded v-bind="attrs" v-on="on">
                     <v-avatar size="25" left>
                         <v-img src="./assets/image_14.png" />
                     </v-avatar>
-                    <span class="font-weight-bold ml-2">Profile</span>
+                    <span class="font-weight-bold ml-2 font-family--poppins">Profile</span>
                     <v-icon right size="24">mdi-chevron-down</v-icon>
                 </v-btn>
             </template>
             <v-list rounded color="dark" dark>
                 <v-list-item v-for="(item, index) in profile" :key="index" route :to="item.route">
                     <v-list-item-content>
-                        <v-list-item-title class="font-weight-bold profile--text--dropdown text-center">{{ item.title }}</v-list-item-title>
+                        <v-list-item-title class="font-weight-bold profile--text--dropdown text-center font-family--poppins">{{ item.title }}</v-list-item-title>
                     </v-list-item-content>
                 </v-list-item>
             </v-list>
@@ -35,7 +35,7 @@
 
     <!-- Drawer Content -->
     <v-navigation-drawer v-model="drawer" app dark color="dark" class="white--text">
-        <v-card-title class="dark font-weight-bold">
+        <v-card-title class="dark font-weight-bold font-family--poppins">
             Songs247x7
         </v-card-title>
 
@@ -44,11 +44,11 @@
         <v-subheader class="font-weight-black grey--text">Menu</v-subheader>
 
         <!-- Menu Section -->
-        <v-btn text block v-for="item in menu" :key="item.text" :to="item.route" class="body-2 py-5 text-none ">
+        <v-btn text block v-for="item in menu" :key="item.text" :to="item.route" class="body-2 py-5 text-none font-family--poppins">
             <span class="bg--grey grey--light--text mr-5 pa-1 rounded-lg">
                 <v-icon size="18" class="icon--white grey--icon" v-text="item.icon"></v-icon>
             </span>
-            <span class="font-weight-medium grey--text text" v-text="item.text">
+            <span class="font-weight-medium grey--text text font-weight-bold font-family--poppins" v-text="item.text">
             </span>
             <v-spacer></v-spacer>
         </v-btn>
@@ -62,11 +62,11 @@
         <!-- Artist Section -->
         <v-subheader class="font-weight-black grey--text">Artist</v-subheader>
 
-        <v-btn text block v-for="(artist, index) in artists" :key="index" :to="artist.route" class="body-2 py-5 text-none ">
+        <v-btn text block v-for="(artist, index) in artists" :key="index" :to="artist.route" class="body-2 py-5 text-none font-family--poppins">
             <span class="bg--grey grey--light--text mr-5 pa-1 rounded-lg ">
                 <v-icon size="18" class="icon--white grey--icon" v-text="artist.icon"></v-icon>
             </span>
-            <span class="font-weight-medium grey--text text" v-text="artist.text">
+            <span class="grey--text text font-weight-bold font-family--poppins" v-text="artist.text">
             </span>
             <v-spacer></v-spacer>
         </v-btn>
@@ -169,7 +169,7 @@ export default {
         font-weight: 500 !important;
         letter-spacing: 0.0071428571em !important;
         line-height: 1.375rem !important;
-        font-family: "Roboto", sans-serif !important;
+        font-family: 'Poppins', sans-serif !important;
     }
 
     .profile--btn {
@@ -179,6 +179,10 @@ export default {
     .profile--text--dropdown {
         font-size: 0.85rem !important;
     }
+}
+
+.font-family--poppins {
+    font-family: 'Poppins', sans-serif !important;
 }
 
 .dark {
@@ -231,5 +235,4 @@ export default {
     background-color: #ff7300 !important;
     color: white !important;
 }
-
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,76 +1,239 @@
 <template>
-<v-container fluid class="dark mt-n5">
+  <v-container fluid class="dark mt-n5">
+    <!-- Discover Section -->
     <v-card dark flat class="dark">
-        <v-card-title class="font-weight-bold text-h5">
-            Discover
-        </v-card-title>
+      <v-card-title class="font-weight-bold text-h4 font-family--poppins">
+        Discover
+      </v-card-title>
     </v-card>
 
-    <!-- <v-row justify="start">
-        <v-col cols="6">
-            <v-card class="rounded-xl">
-                <v-img src=".././assets/image_1.png">
+    <v-row>
+      <v-col cols="12" sm="7">
+        <v-card id="create" class="rounded-xl">
+          <v-img
+            src="./../assets/image_1.png"
+            min-height="350"
+            max-height="350"
+          >
+            <v-speed-dial bottom="bottom" right="right">
+              <template v-slot:activator>
+                <v-chip
+                  small
+                  color="#3d414d"
+                  class="white--text font-weight-bold font-family--poppins"
+                  >7:30 min</v-chip
+                >
+              </template>
+            </v-speed-dial>
+            <v-speed-dial bottom left>
+              <template v-slot:activator>
+                <div class="avatar--border-1 pa-2 ">
+                  <v-avatar size="50">
+                    <v-img src="./../assets/image_2.png"></v-img>
+                  </v-avatar>
+                  <span class="position--absolute rounded-pill border--card--1">
+                    <v-icon color="white">mdi-check-circle</v-icon>
+                  </span>
+                </div>
+              </template>
+            </v-speed-dial>
+          </v-img>
+        </v-card>
+      </v-col>
 
-                    <span class="font-weight-bold white--text text-h5">How to do Basic Jumping and how to landing safely</span>
+      <v-col cols="12" sm="5">
+        <v-card id="create" class="rounded-xl">
+          <v-img
+            src="./../assets/image_3.png"
+            min-height="350"
+            max-height="350"
+          >
+            <v-speed-dial bottom="bottom" right="right">
+              <template v-slot:activator>
+                <v-chip
+                  small
+                  color="#3d414d"
+                  class="white--text font-weight-bold font-family--poppins"
+                  >12:10 min</v-chip
+                >
+              </template>
+            </v-speed-dial>
 
-                    <v-card flat class="transparent">
-                        <v-row>
-                            <v-col cols="6" class="border">
-                                <v-card-title>
-                                    <p class="font-weight-bold white--text text-justify text-h4 mt-6 pa-2">
-                                        How to do Basic Jumping and how to landing safely
-                                    </p>
-                                </v-card-title>
-                            </v-col>
+            <v-speed-dial bottom left>
+              <template v-slot:activator>
+                <div class="avatar--border-2 pa-2">
+                  <v-avatar size="50">
+                    <v-img src="./../assets/image_4.png"></v-img>
+                  </v-avatar>
+                  <span class="position--absolute rounded-pill border--card--2">
+                    <v-icon color="white">mdi-check-circle</v-icon>
+                  </span>
+                </div>
+              </template>
+            </v-speed-dial>
+          </v-img>
+        </v-card>
+      </v-col>
+    </v-row>
 
-                            <v-col cols="12">
-                                <v-card-title>
-                                    <v-avatar size="56" class="round--circle">
-                                        <img alt="user" src=".././assets/image_2.png">
-                                    </v-avatar>
+    <!-- Most Listened Section -->
 
-                                    <v-row>
-                                        <v-col cols="12">
-                                            <p class="ml-3 white--text">
-                                                Thomas Hope
-                                            </p>
-                                        </v-col>
-                                    </v-row>
-                                </v-card-title>
-                            </v-col>
+    <v-card dark flat class="dark mt-2">
+      <v-card-title class="headline font-family--poppins">
+        Most Listened
+      </v-card-title>
+    </v-card>
 
-                            <v-col cols="12">
-                                <v-card-actions>
-                                    <v-spacer></v-spacer>
-                                    <v-chip color="#3d414d" class="white--text font-weight-bold body-1 ma-5">
-                                        7:10 min
-                                    </v-chip>
-                                </v-card-actions>
-                            </v-col>
-                        </v-row>
-                    </v-card>
-                </v-img>
-            </v-card>
-        </v-col>
-    </v-row> -->
+    <v-row>
+      <v-col cols="12" sm="6" lg="3" v-for="(card, index) in cards" :key="index">
+        <v-card id="create" class="rounded-xl darkest">
+          <v-img src="./../assets/image_5.png" height="200px">
+            <v-speed-dial top="top" right="right">
+              <template v-slot:activator>
+                <v-chip
+                  small
+                  color="#3d414d"
+                  class="white--text font-weight-bold font-family--poppins"
+                  >{{ card.time }} </v-chip
+                >
+              </template>
+            </v-speed-dial>
+          </v-img>
 
+          <v-speed-dial top right class="position--down">
+            <template v-slot:activator>
+              <div class="avatar--border-3 pa-2">
+                <v-avatar size="50">
+                  <v-img src="./../assets/image_13.png"></v-img>
+                </v-avatar>
+                <span class="position--absolute rounded-pill border--card--3">
+                  <v-icon color="blue">mdi-check-circle</v-icon>
+                </span>
+              </div>
+            </template>
+          </v-speed-dial>
 
+          <v-card-title>
+            <p class="grey--text body-1 font-family--poppins">
+              {{ card.name }}
+              <v-badge dot :color="card.color" class="mx-2 mb-2" inline></v-badge>
+            </p>
+          </v-card-title>
 
-</v-container>
+          <v-card-subtitle class="mt-n7">
+            <p class="white--text title font-family--poppins">
+              {{ card.title }}
+            </p>
+          </v-card-subtitle>
+
+          <v-card-actions class="mt-n5 ml-2">
+            <p class="grey--text body-2 font-family--poppins">
+              {{ card.text }}
+            </p>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
+<script>
+export default {
+  data: () => ({
+    cards: [
+      {
+        name: "Andy William",
+        title: "Basic how to ride your skateboard comfortly",
+        text: "53K plays . 2 week ago",
+        time: "7 min",
+        color: "green",
+        image: "./../assets/image_5.png",
+        avatar: "./../assets/image_10.png"
+      },
+      {
+        name: "Johny Wise",
+        title: "Prepare for your first skateboard jump",
+        text: "53K plays . 2 week ago",
+        time: "7 min",
+        color: "red",
+        image: "./../assets/image_7.png",
+        avatar: "./../assets/image_12.png"
+      },
+      {
+        name: "Budi Hakim",
+        title: "Tips to playing skateboard on the ramp",
+        text: "53K plays . 2 week ago",
+        time: "7 min",
+        color: "red",
+        image: "./../assets/image_6.png",
+        avatar: "./../assets/image_11.png"
+      },
+      {
+        name: "Wijaya Abadi",
+        title: "Basic Equipment to play skateboard safely",
+        text: "53K plays . 2 week ago",
+        time: "7 min",
+        color: "green",
+        image: "./../assets/image_8.png",
+        avatar: "./../assets/image_13.png"
+      },
+    ],
+  }),
+};
+</script>
+
 <style scoped>
-.transparent {
-    background-color: transparent !important;
+.avatar--border-1 {
+  border: 2px solid #09c6db;
+  border-radius: 50%;
 }
 
-/*
-.border {
-    border: 1px solid red;
-} */
+.avatar--border-2 {
+  border: 2px solid #ead5b4;
+  border-radius: 50%;
+}
 
-/* .round--circle {
-    border: 1px solid grey;
+.avatar--border-3 {
+  border: 1px solid #ffffff;
+  border-radius: 50%;
+}
 
-} */
+.position--absolute {
+  position: absolute;
+  top: 50px;
+  right: 0px;
+}
+
+#create .v-speed-dial {
+  position: absolute;
+}
+
+.border--card--1 {
+  border: 1px solid #50b7ca;
+  background-color: #50b7ca;
+}
+
+.border--card--2 {
+  border: 1px solid #ebc080;
+  background-color: #ebc080;
+}
+
+.border--card--3 {
+  border: 1px solid #ffffff;
+  background-color: #ffffff;
+}
+
+.darkest {
+  background-color: #202b38;
+}
+
+.font-family--poppins {
+  font-family: "Poppins", sans-serif !important;
+}
+
+.position--down {
+  top: 165px !important;
+}
 </style>
+
+
