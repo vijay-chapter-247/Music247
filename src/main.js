0c84192ec48611e4a78ec9bd3c77834c9298e7c2
Vijay <vijay.v@chapter247.com>
2021-08-03 20:30:32 +0530

    Home, Trending, Playlist, Albums data from api using vuex

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
-// import { store } from "./store/index";
-import store from "../store/index";
+import { store } from "../store/index";
+// import store from "../store/index";
 
 import Spotify from "spotify-web-api-node";
 import VueSpotify from "vue-spotify";
